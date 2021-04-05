965fd5db955729c3d1cd4688375028f6cba122ab
frutabruta <adameichr1@gmail.com>
2021-04-05 02:44:48 +0200

    oddeleni VDV301 publisher do git submodulu

--- VDV301publisher/customerinformationservice.cpp
+++
@@ -1,8 +0,0 @@
-#include "customerinformationservice.h"
-#include "httpsluzba.h"
-
-
-CustomerInformationService::CustomerInformationService(QString nazevSluzby, QString typSluzby, int cisloPortu,QString verze):HttpSluzba( nazevSluzby,typSluzby, cisloPortu,verze)
-{
-
-}
