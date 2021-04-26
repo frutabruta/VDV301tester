f745508b72af7a1722e943349cc1d7922de45f7e
frutabruta <adameichr1@gmail.com>
2021-04-27 00:09:27 +0200

    změna názvu služby CIS ve verzi 2.2CZ1.0 na CIS(2) pro zabránění kolize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,7 +14,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow),
     deviceManagementService1_0("DeviceManagementService","_ibisip_http._tcp",47477,"1.0"),
     customerInformationService1_0("CustomerInformationService","_ibisip_http._tcp",47479,"1.0"),
-    customerInformationService2_2CZ1_0("CustomerInformationService","_ibisip_http._tcp",47480,"2.2CZ1.0")
+    customerInformationService2_2CZ1_0("CustomerInformationService(2)","_ibisip_http._tcp",47480,"2.2CZ1.0")
 {
     qDebug()<<"MainWindow::MainWindow";
     ui->setupUi(this);
